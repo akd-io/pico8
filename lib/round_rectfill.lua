a1359028a6baf80485178baa383471e00900d643
Anders Kjær Damgaard <akd@akd.io>
2024-11-16 00:07:41 +0100

    Format files

--- lib/round_rectfill.lua
+++ lib/round_rectfill.lua
@@ -1,6 +1,6 @@
 -- roundRectfill() takes the same parameters as rectfill(), and draws a similar
 -- rectangle, but without the pixels at the corners to simulate a rounded rectangle.
-function roundRectfill( x0, y0, x1, y1, col )
-  rectfill(x0+1, y0, x1-1, y1,col)
-  rectfill(x0, y0+1, x1, y1-1,col)
+function roundRectfill(x0, y0, x1, y1, col)
+  rectfill(x0 + 1, y0, x1 - 1, y1, col)
+  rectfill(x0, y0 + 1, x1, y1 - 1, col)
 end
