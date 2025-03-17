5aef15c9c9ce9885a59844fa568ba91f6fc360a0
Anders Kjær Damgaard <akd@akd.io>
2025-03-18 00:34:35 +0100

    Fix isArray function

--- lib/array_methods.lua
+++ lib/array_methods.lua
@@ -1,32 +1,29 @@
 -- isArray returns true if the value is a table and all keys are consecutive numbers.
-function isArray(value)
-  if (type(value) != "table") then
-    return false
-  end
-  local arrayLength = #value
-  for k in pairs(value) do
-    if (type(k) != "number" or k > arrayLength) then
-      return false
-    end
+local function isArray(table)
+  if (type(table) != "table") then return false end
+  local i = 0
+  for _ in pairs(table) do
+    i += 1
+    if table[i] == nil then return false end
   end
   return true
 end
 
-function map(table, callback, iterator)
+local function map(table, callback, iterator)
   local newTable = {}
   for i, v in iterator(table) do
     newTable[i] = callback(v, i)
   end
   return newTable
 end
-function arrayMap(array, callback)
+local function arrayMap(array, callback)
   return map(array, callback, ipairs)
 end
-function objectMap(object, callback)
+local function objectMap(object, callback)
   return map(object, callback, pairs)
 end
 
-function filter(table, callback, iterator)
+local function filter(table, callback, iterator)
   local newTable = {}
   for i, v in iterator(table) do
     if callback(v, i) then
@@ -35,29 +32,29 @@ function filter(table, callback, iterator)
   end
   return newTable
 end
-function arrayFilter(array, callback)
+local function arrayFilter(array, callback)
   return filter(array, callback, ipairs)
 end
-function objectFilter(object, callback)
+local function objectFilter(object, callback)
   return filter(object, callback, pairs)
 end
 
-function __reduce(table, callback, initialValue, iterator)
+local function __reduce(table, callback, initialValue, iterator)
   local accumulator = initialValue
   for i, v in iterator(table) do
     accumulator = callback(accumulator, v, i)
   end
   return accumulator
 end
-function arrayReduce(array, callback, initialValue)
+local function arrayReduce(array, callback, initialValue)
   return __reduce(array, callback, initialValue, ipairs)
 end
 
-function objectReduce(object, callback, initialValue)
+local function objectReduce(object, callback, initialValue)
   return __reduce(object, callback, initialValue, pairs)
 end
 
-function find(table, callback, iterator)
+local function find(table, callback, iterator)
   for i, v in iterator(table) do
     if callback(v, i) then
       return v, i
@@ -65,14 +62,14 @@ function find(table, callback, iterator)
   end
   return nil
 end
-function arrayFind(array, callback)
+local function arrayFind(array, callback)
   return find(array, callback, ipairs)
 end
-function objectFind(object, callback)
+local function objectFind(object, callback)
   return find(object, callback, pairs)
 end
 
-function __findIndex(table, callback, iterator)
+local function __findIndex(table, callback, iterator)
   for i, v in iterator(table) do
     if callback(v, i) then
       return i
@@ -80,14 +77,14 @@ function __findIndex(table, callback, iterator)
   end
   return nil
 end
-function arrayFindIndex(array, callback)
+local function arrayFindIndex(array, callback)
   return __findIndex(array, callback, ipairs)
 end
-function objectFindIndex(object, callback)
+local function objectFindIndex(object, callback)
   return __findIndex(object, callback, pairs)
 end
 
-function __every(table, callback, iterator)
+local function __every(table, callback, iterator)
   for i, v in iterator(table) do
     if not callback(v, i) then
       return false
@@ -95,14 +92,14 @@ function __every(table, callback, iterator)
   end
   return true
 end
-function arrayEvery(array, callback)
+local function arrayEvery(array, callback)
   return __every(array, callback, ipairs)
 end
-function objectEvery(object, callback)
+local function objectEvery(object, callback)
   return __every(object, callback, pairs)
 end
 
-function __some(table, callback, iterator)
+local function __some(table, callback, iterator)
   for i, v in iterator(table) do
     if callback(v, i) then
       return true
@@ -110,14 +107,14 @@ function __some(table, callback, iterator)
   end
   return false
 end
-function arraySome(array, callback)
+local function arraySome(array, callback)
   return __some(array, callback, ipairs)
 end
-function objectSome(object, callback)
+local function objectSome(object, callback)
   return __some(object, callback, pairs)
 end
 
-function __includes(table, value, iterator)
+local function __includes(table, value, iterator)
   for i, v in iterator(table) do
     if v == value then
       return true
@@ -125,14 +122,14 @@ function __includes(table, value, iterator)
   end
   return false
 end
-function arrayIncludes(array, value)
+local function arrayIncludes(array, value)
   return __includes(array, value, ipairs)
 end
-function objectIncludes(object, value)
+local function objectIncludes(object, value)
   return __includes(object, value, pairs)
 end
 
-function __indexOf(table, value, iterator)
+local function __indexOf(table, value, iterator)
   for i, v in iterator(table) do
     if v == value then
       return i
@@ -140,14 +137,14 @@ function __indexOf(table, value, iterator)
   end
   return -1
 end
-function arrayIndexOf(array, value)
+local function arrayIndexOf(array, value)
   return __indexOf(array, value, ipairs)
 end
-function objectIndexOf(object, value)
+local function objectIndexOf(object, value)
   return __indexOf(object, value, pairs)
 end
 
-function arrayLastIndexOf(array, value)
+local function arrayLastIndexOf(array, value)
   for i = #array, 1, -1 do
     if array[i] == value then
       return i
@@ -156,58 +153,58 @@ function arrayLastIndexOf(array, value)
   return -1
 end
 
-function __keys(table, iterator)
+local function __keys(table, iterator)
   local keys = {}
   for k, _ in iterator(table) do
     table.insert(keys, k)
   end
   return keys
 end
-function arrayKeys(array)
+local function arrayKeys(array)
   return __keys(array, ipairs)
 end
-function objectKeys(object)
+local function objectKeys(object)
   return __keys(object, pairs)
 end
 
-function __values(table, iterator)
+local function __values(table, iterator)
   local values = {}
   for _, v in iterator(table) do
     table.insert(values, v)
   end
   return values
 end
-function arrayValues(array)
+local function arrayValues(array)
   return __values(array, ipairs)
 end
-function objectValues(object)
+local function objectValues(object)
   return __values(object, pairs)
 end
 
-function __entries(table, iterator)
+local function __entries(table, iterator)
   local entries = {}
   for k, v in iterator(table) do
     table.insert(entries, { k, v })
   end
   return entries
 end
-function arrayEntries(array)
+local function arrayEntries(array)
   return __entries(array, ipairs)
 end
-function objectEntries(object)
+local function objectEntries(object)
   return __entries(object, pairs)
 end
 
-function arrayFrom(iterator)
+local function arrayFrom(iterator)
   assert(false, "arrayFrom is not implemented")
   -- TODO: Find out how to use iterators in lua. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
 end
 
-function arraySlice(array, start, end_)
+local function arraySlice(array, start, end_)
   assert(false, "Use sub() instead.")
 end
 
-function arraySplice(array, start, deleteCount, ...)
+local function arraySplice(array, start, deleteCount, ...)
   local removed = {}
   for i = start, start + deleteCount - 1 do
     array.insert(removed, array[i])
@@ -223,20 +220,20 @@ function arraySplice(array, start, deleteCount, ...)
   return removed
 end
 
-function arrayPush(array, ...)
+local function arrayPush(array, ...)
   for i = 1, select("#", ...) do
     array[#array + 1] = select(i, ...)
   end
   return #array
 end
 
-function arrayPop(array)
+local function arrayPop(array)
   local value = array[#array]
   array[#array] = nil
   return value
 end
 
-function arrayUnshift(array, ...)
+local function arrayUnshift(array, ...)
   for i = select("#", ...), 1, -1 do
     array[#array + 1] = array[i]
     array[i] = select(i, ...)
@@ -244,7 +241,7 @@ function arrayUnshift(array, ...)
   return #array
 end
 
-function arrayShift(array)
+local function arrayShift(array)
   local value = array[1]
   for i = 1, #array - 1 do
     array[i] = array[i + 1]
