3e8e806afff048caed9b2c4f95d3d0bfdfd453c2
Anders Kjær Damgaard <akd@akd.io>
2025-03-17 14:01:29 +0100

    Update array methods

--- lib/array_methods.lua
+++ lib/array_methods.lua
@@ -1,178 +1,254 @@
-function arrayMap(table, callback)
+function map(table, callback, iterator)
   local newTable = {}
-  for i, v in pairs(table) do
+  for i, v in iterator(table) do
     newTable[i] = callback(v, i)
   end
   return newTable
 end
+function arrayMap(array, callback)
+  return map(array, callback, ipairs)
+end
+function objectMap(object, callback)
+  return map(object, callback, pairs)
+end
 
-function arrayFilter(table, callback)
+function filter(table, callback, iterator)
   local newTable = {}
-  for i, v in pairs(table) do
+  for i, v in iterator(table) do
     if callback(v, i) then
       table.insert(newTable, v)
     end
   end
   return newTable
 end
+function arrayFilter(array, callback)
+  return filter(array, callback, ipairs)
+end
+function objectFilter(object, callback)
+  return filter(object, callback, pairs)
+end
 
-function arrayReduce(table, callback, initialValue)
+function __reduce(table, callback, initialValue, iterator)
   local accumulator = initialValue
-  for i, v in pairs(table) do
+  for i, v in iterator(table) do
     accumulator = callback(accumulator, v, i)
   end
   return accumulator
 end
+function arrayReduce(array, callback, initialValue)
+  return __reduce(array, callback, initialValue, ipairs)
+end
 
-function arrayFind(table, callback)
-  for i, v in pairs(table) do
+function objectReduce(object, callback, initialValue)
+  return __reduce(object, callback, initialValue, pairs)
+end
+
+function find(table, callback, iterator)
+  for i, v in iterator(table) do
     if callback(v, i) then
       return v, i
     end
   end
   return nil
 end
+function arrayFind(array, callback)
+  return find(array, callback, ipairs)
+end
+function objectFind(object, callback)
+  return find(object, callback, pairs)
+end
 
-function arrayFindIndex(table, callback)
-  for i, v in pairs(table) do
+function __findIndex(table, callback, iterator)
+  for i, v in iterator(table) do
     if callback(v, i) then
       return i
     end
   end
   return nil
 end
+function arrayFindIndex(array, callback)
+  return __findIndex(array, callback, ipairs)
+end
+function objectFindIndex(object, callback)
+  return __findIndex(object, callback, pairs)
+end
 
-function arrayEvery(table, callback)
-  for i, v in pairs(table) do
+function __every(table, callback, iterator)
+  for i, v in iterator(table) do
     if not callback(v, i) then
       return false
     end
   end
   return true
 end
+function arrayEvery(array, callback)
+  return __every(array, callback, ipairs)
+end
+function objectEvery(object, callback)
+  return __every(object, callback, pairs)
+end
 
-function arraySome(table, callback)
-  for i, v in pairs(table) do
+function __some(table, callback, iterator)
+  for i, v in iterator(table) do
     if callback(v, i) then
       return true
     end
   end
   return false
 end
+function arraySome(array, callback)
+  return __some(array, callback, ipairs)
+end
+function objectSome(object, callback)
+  return __some(object, callback, pairs)
+end
 
-function arrayIncludes(table, value)
-  for i, v in pairs(table) do
+function __includes(table, value, iterator)
+  for i, v in iterator(table) do
     if v == value then
       return true
     end
   end
   return false
 end
+function arrayIncludes(array, value)
+  return __includes(array, value, ipairs)
+end
+function objectIncludes(object, value)
+  return __includes(object, value, pairs)
+end
 
-function arrayIndexOf(table, value)
-  for i, v in pairs(table) do
+function __indexOf(table, value, iterator)
+  for i, v in iterator(table) do
     if v == value then
       return i
     end
   end
   return -1
 end
+function arrayIndexOf(array, value)
+  return __indexOf(array, value, ipairs)
+end
+function objectIndexOf(object, value)
+  return __indexOf(object, value, pairs)
+end
 
-function arrayLastIndexOf(table, value)
-  for i = #table, 1, -1 do
-    if table[i] == value then
+function arrayLastIndexOf(array, value)
+  for i = #array, 1, -1 do
+    if array[i] == value then
       return i
     end
   end
   return -1
 end
 
-function arrayKeys(table)
+function __keys(table, iterator)
   local keys = {}
-  for k, _ in pairs(table) do
+  for k, _ in iterator(table) do
     table.insert(keys, k)
   end
   return keys
 end
+function arrayKeys(array)
+  return __keys(array, ipairs)
+end
+function objectKeys(object)
+  return __keys(object, pairs)
+end
 
-function arrayValues(table)
+function __values(table, iterator)
   local values = {}
-  for _, v in pairs(table) do
+  for _, v in iterator(table) do
     table.insert(values, v)
   end
   return values
 end
+function arrayValues(array)
+  return __values(array, ipairs)
+end
+function objectValues(object)
+  return __values(object, pairs)
+end
 
-function arrayEntries(table)
+function __entries(table, iterator)
   local entries = {}
-  for k, v in pairs(table) do
+  for k, v in iterator(table) do
     table.insert(entries, { k, v })
   end
   return entries
 end
+function arrayEntries(array)
+  return __entries(array, ipairs)
+end
+function objectEntries(object)
+  return __entries(object, pairs)
+end
 
-function arrayFrom(table)
-  local newTable = {}
-  for i, v in pairs(table) do
-    newTable[i] = v
+function arrayFrom(iterator)
+  assert(false, "arrayFrom is not implemented")
+  -- TODO: Find out how to use iterators in lua. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+end
+
+-- isArray returns true if the value is a table and all keys are consecutive numbers.
+function isArray(value)
+  if (type(value) != "table") then
+    return false
   end
-  return newTable
-end
-
-function arrayIsArray(table)
-  return type(table) == "table" and #table > 0 and #table == table.n
-end
-
-function arraySlice(table, start, end_)
-  local newTable = {}
-  for i = start or 1, end_ or #table do
-    table.insert(newTable, table[i])
+  local arrayLength = #value
+  for k in pairs(value) do
+    if (type(k) != "number" or k > arrayLength) then
+      return false
+    end
   end
-  return newTable
+  return true
 end
 
-function arraySplice(table, start, deleteCount, ...)
+function arraySlice(array, start, end_)
+  assert(false, "Use sub() instead.")
+end
+
+function arraySplice(array, start, deleteCount, ...)
   local removed = {}
   for i = start, start + deleteCount - 1 do
-    table.insert(removed, table[i])
-    table[i] = nil
+    array.insert(removed, array[i])
+    array[i] = nil
   end
   for i = start, start + select("#", ...) - 1 do
-    table[i] = select(i - start + 1, ...)
+    array[i] = select(i - start + 1, ...)
   end
-  for i = start + select("#", ...), #table do
-    table[i - select("#", ...)] = table[i]
-    table[i] = nil
+  for i = start + select("#", ...), #array do
+    array[i - select("#", ...)] = array[i]
+    array[i] = nil
   end
   return removed
 end
 
-function arrayPush(table, ...)
+function arrayPush(array, ...)
   for i = 1, select("#", ...) do
-    table[#table + 1] = select(i, ...)
+    array[#array + 1] = select(i, ...)
   end
-  return #table
+  return #array
 end
 
-function arrayPop(table)
-  local value = table[#table]
-  table[#table] = nil
+function arrayPop(array)
+  local value = array[#array]
+  array[#array] = nil
   return value
 end
 
-function arrayUnshift(table, ...)
+function arrayUnshift(array, ...)
   for i = select("#", ...), 1, -1 do
-    table[#table + 1] = table[i]
-    table[i] = select(i, ...)
+    array[#array + 1] = array[i]
+    array[i] = select(i, ...)
   end
-  return #table
+  return #array
 end
 
-function arrayShift(table)
-  local value = table[1]
-  for i = 1, #table - 1 do
-    table[i] = table[i + 1]
+function arrayShift(array)
+  local value = array[1]
+  for i = 1, #array - 1 do
+    array[i] = array[i + 1]
   end
-  table[#table] = nil
+  array[#array] = nil
   return value
 end
