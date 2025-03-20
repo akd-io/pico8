52c7d099d6527a95bcbea76959fff8d61e917022
Anders Kjær Damgaard <akd@akd.io>
2025-03-20 23:10:37 +0100

    Update rnd32 docs

--- lib/rnd32.lua
+++ lib/rnd32.lua
@@ -1,8 +1,11 @@
--- rnd32() returns a random number between 0x0000.0000 and 0xffff.fffe, inclusive, by calling rnd(~0), equivalent to rnd(0xffff.ffff).
--- The specific value 0xffff.ffff is excluded because the rnd() function's limit argument is exclusive.
--- Many developers use rnd(-1) but this translates to rnd(0xffff) and excludes 0x0000ffff possibilities instead of just 0x00000001.
--- An alternate version that supports 0xffff.ffff can be implented with two rnd()-calls, but is slower and not used here.
--- Devs should use rnd(~0) directly to save a function call, but it's here for documentation.
+-- rnd32() returns a random number between 0x0000.0000 and 0xffff.fffe,
+-- inclusive.
+-- It does this by calling rnd(~0), as ~0 = bnot(0x0000.0000) = 0xffff.ffff
+-- 0xffff.fffe is the highest the rnd() function will go, as it's max argument
+-- is exclusive.
+-- Some use rnd(-1), but rnd(~0) is superior as -1 is only 0xffff.0000.
+-- Devs should use rnd(~0) directly to save a function call, but rnd32() is
+-- here for documentation purposes.
 function rnd32()
   return rnd(~0)
 end
