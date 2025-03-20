44def64ada85233f737f5cd0e4d72d83457b712a
Anders Kjær Damgaard <akd@akd.io>
2025-03-20 15:09:28 +0100

    Purge underscores 2

--- projects/rpg/lib/generate-item.lua
+++ projects/rpg/lib/generate-item.lua
@@ -1,16 +1,16 @@
 function generateItem()
-  local equipment_type = rnd(item_types)
-  local availableAffixesBag = shallow_copy(affix_type_keys)
-  local numAffixes = ceil(rnd(max_affixes_per_item))
+  local equipmentType = rnd(itemTypes)
+  local availableAffixesBag = shallowCopy(affixTypeKeys)
+  local numAffixes = ceil(rnd(maxAffixesPerItem))
   local affixes = {}
   for i = 1, numAffixes do
-    local affix_type = rnd(availableAffixesBag)
-    local tier = rnd(affix_tiers)
-    affixes[affix_type] = tier
-    del(availableAffixesBag, affix_type)
+    local affixType = rnd(availableAffixesBag)
+    local tier = rnd(affixTiers)
+    affixes[affixType] = tier
+    del(availableAffixesBag, affixType)
   end
   return {
-    equipment_type = equipment_type,
+    equipmentType = equipmentType,
     affixes = affixes
   }
 end
