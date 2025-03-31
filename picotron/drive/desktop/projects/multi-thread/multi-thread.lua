a8b8c872fa1b586b955bdd9f37dd0b83abebf03c
Anders Kjær Damgaard <akd@akd.io>
2025-03-31 19:42:18 +0200

    Add multi-thread wip

---
+++ picotron/drive/desktop/projects/multi-thread/multi-thread.lua
@@ -0,0 +1,58 @@
+--[[pod_format="raw",created="2025-03-30 21:12:50",modified="2025-03-31 11:24:42",revision=10]]
+
+window({
+  width = 140,
+  height = 80,
+  resizeable = true,
+  moveable = true,
+  has_frame = true,
+  title = tostr(pid())
+})
+
+local children = 4
+local childPids = {}
+
+function _init()
+  local prog_name = "../hello-world/main.lua"
+  local val = create_process(prog_name)
+  notify(tostr(val))
+end
+
+local minMag, mag, maxMag = 1, 1, 22
+local btnUp, btnDown = 2, 3
+
+local update = 0
+
+function _update()
+  update += 1
+
+  if btnp(btnUp) then
+    print("btnUp pressed")
+    mag = mid(minMag, mag + 1, maxMag)
+  end
+  if btnp(btnDown) then
+    mag = mid(minMag, mag - 1, maxMag)
+  end
+end
+
+local draw = 0
+function _draw()
+  draw += 1
+
+  local n = 2 ^ mag
+
+  for i = 1, n do
+    -- Do nothing.
+  end
+
+  cls()
+  print("processId: " .. pid())
+  print("pwd: " .. tostr(pwd()))
+  print("pwf: " .. tostr(pwf()))
+  print("draw: " .. draw)
+  print("update: " .. update)
+  print("CPU: " .. stat(1))
+  print("FPS: " .. stat(7))
+  print("mag: " .. mag)
+  print("n: " .. n)
+end
