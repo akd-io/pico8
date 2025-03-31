4a96b59094e53fc953b10ccba0ce6ea3d4dc1538
Anders Kjær Damgaard <akd@akd.io>
2025-03-31 21:30:03 +0200

    Update dump

--- picotron/drive/desktop/projects/dump/dump.lua
+++ picotron/drive/desktop/projects/dump/dump.lua
@@ -1,4 +1,6 @@
 --[[pod_format="raw",created="2025-03-31 15:08:47",modified="2025-03-31 15:08:47",revision=0]]
 
-cp("/ram", "/dumps/ram")
+rm("/dumps/system")
 cp("/system", "/dumps/system")
+rm("/dumps/ram")
+cp("/ram", "/dumps/ram")
