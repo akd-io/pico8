db21a582dc40ccbab6ae9d04ce6e91abe291d7a3
Anders Kjær Damgaard <akd@akd.io>
2025-03-31 21:41:34 +0200

    Add builtins and env to dump

--- picotron/drive/desktop/projects/dump/dump.lua
+++ picotron/drive/desktop/projects/dump/dump.lua
@@ -1,6 +1,17 @@
 --[[pod_format="raw",created="2025-03-31 15:08:47",modified="2025-03-31 15:08:47",revision=0]]
 
+print("Dumping...")
+
+rm("/dumps/builtins.txt")
+create_process("/desktop/projects/builtins/builtins.lua", { argv = { "/dumps/builtins.txt" } })
+
+rm("/dumps/env.txt")
+create_process("/desktop/projects/env/env.lua", { argv = { "/dumps/env.txt" } })
+
 rm("/dumps/system")
 cp("/system", "/dumps/system")
+
 rm("/dumps/ram")
 cp("/ram", "/dumps/ram")
+
+print("Done.")
