523e631d67b203a60699b6279393ccb51053b26e
Anders Kjær Damgaard <akd@akd.io>
2025-04-01 11:09:02 +0200

    Remove arrayToString and objectToString from Picotron utils

--- picotron/drive/lib/utils.lua
+++ picotron/drive/lib/utils.lua
@@ -65,49 +65,6 @@ function join(elements, separator)
   return result
 end
 
-function arrayToString(array)
-  return "{ " .. join(array) .. " }"
-end
-
-function objectToString(object, indent)
-  depth = depth or 0
-
-  local function getIndent(d)
-    local spaces = ""
-    for i = 1, d * 2 do
-      spaces = spaces .. " "
-    end
-    return spaces
-  end
-
-  local indent = getIndent(depth)
-  local strings = {}
-
-  add(strings, "{")
-  local first = true
-
-  for key, value in pairs(object) do
-    if not first then
-      add(strings, ",")
-    end
-    first = false
-
-    add(strings, "\n" .. indent .. "  " .. key .. ": ")
-
-    if type(value) == "table" then
-      add(strings, objectToString(value, depth + 1))
-    else
-      add(strings, tostr(value))
-    end
-  end
-
-  if #strings > 1 then
-    add(strings, "\n" .. indent)
-  end
-  add(strings, "}")
-
-  return join(strings, "")
-end
 
 arrayForeach = foreach
 function objectForeach(object, callback)
