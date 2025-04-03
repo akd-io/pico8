a9a73f8d2138669d7999dfda018e9d806926cede
Anders Kjær Damgaard <akd@akd.io>
2025-04-03 20:41:35 +0200

    Add raytron.lua with TODOs

---
+++ picotron/drive/projects/raytron/raytron.lua
@@ -0,0 +1,16 @@
+--[[
+  TODOs:
+  - Make a Raycast clone.
+  - Find out, if it's possible to make a wm.lua patch to support all processes,
+    focused or not, to listen for keyboard events.
+  - Research possibility for extension support.
+    - Apps should be able to hook into the user input, and add their own items
+      to the dropdown.
+  - Official support for:
+    - Searching files and folders
+    - Opening files and folders (Enter)
+    - Open file location (CMD + Enter)
+    - Opening files with app different from default
+    - Calculator
+    - One-line lua repl using `>` prefix?
+]]
