84ff68d751ec47c2bf55e9a1fb25ebfb0fd6289c
Anders Kjær Damgaard <akd@akd.io>
2025-04-05 02:35:47 +0200

    Add first test files

--- picotron/drive/projects/test/lib/test.lua
+++ picotron/drive/projects/test/lib/test.lua
@@ -1,1 +1,67 @@
--- Testing library
+--[[
+  Testing library
+  Basically a port of https://bun.sh/docs/test/writing
+
+  TODOs:
+  - Implenent
+]]
+
+local groups = {}
+
+local stack = {}
+
+---describe() is a function that groups related tests together.
+---
+---@param groupDescription string
+---@param callback function
+function describe(groupDescription, callback)
+  local group = {
+    type = "group",
+    description = groupDescription,
+  }
+  add(stack, group)
+  callback()
+  deli(stack, #stack)
+end
+
+---test() is a function that defines a single test.
+---
+---A simple example:
+---```lua
+---test("2 + 2", () => {
+---  expect(2 + 2).toBe(4);
+---});
+---```
+---
+---@param testDescription string
+---@param callback function
+function test(testDescription, callback)
+  add(tests, callback)
+end
+
+function beforeEach(callback)
+  error("Implement")
+end
+
+function beforeAll(callback)
+  error("Implement")
+end
+
+function expect(actual)
+  local function toBe(expected)
+    --- shallow equals
+    if actual ~= expected then
+      error("Expected " .. actual .. " to be " .. expected)
+    end
+  end
+
+  local function toEqual(expected)
+    -- deep equals
+    error("Implement")
+  end
+
+  return {
+    toBe = toBe,
+    toEqual = toEqual
+  }
+end
---
+++ picotron/drive/projects/test/tests/manual.lua
@@ -0,0 +1,5 @@
+--include("../lib/test.lua")
+
+test("2 + 2", function()
+  expect(2 + 2).toBe(4)
+end)
