a04516c7c44d4332d0f02ac3f5eece51b54d8b96
Anders Kjær Damgaard <akd@akd.io>
2025-04-07 16:40:29 +0200

    Remove `env()` from `t` command.

--- picotron/drive/appdata/system/util/t.lua
+++ picotron/drive/appdata/system/util/t.lua
@@ -1,2 +1,2 @@
 --[[pod_format="raw",created="2025-04-01 07:44:58",modified="2025-04-01 07:45:01",revision=1]]
-create_process("/system/apps/terminal.lua", env())
+create_process("/system/apps/terminal.lua")
