0e52ee37f276fd3f6dd5bcb308378438ec932008
Anders Kjær Damgaard <akd@akd.io>
2025-04-08 13:52:40 +0200

    Add version todos

---
+++ picotron/versions/diff-env-outputs.ts
@@ -0,0 +1,1 @@
+// TODO: Implement script to diff env outputs, to visualize what changed from version to version.
---
+++ picotron/versions/diff-manuals.ts
@@ -0,0 +1,1 @@
+// TODO: Implement script to diff `picotron_manual.txt` files to visualize what changed from version to version.
