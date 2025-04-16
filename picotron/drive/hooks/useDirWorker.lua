a3ce67b034ced679fe1cec6a8b42f752e7f3807b
Anders Kjær Damgaard <akd@akd.io>
2025-04-16 23:49:35 +0200

    Add TODOs to useDirWorker

--- picotron/drive/hooks/useDirWorker.lua
+++ picotron/drive/hooks/useDirWorker.lua
@@ -1,5 +1,15 @@
 local e = env()
 local path = e.argv[1]
+
+-- TODO: Refactor into a general purpose worker.
+-- TODO: Worker takes `funcName` string, `args` array, and `return` boolean.
+-- TODO: Runs `funcName` with `args` and messages the parent process with the
+-- TODO: result before exiting if `return` is true.
+-- TODO: Event will include `funcName`, `args`, and `packedResult`, where `packedResult` is the result of `pack(result)`.
+-- TODO: IMPORTANT: What DX makes it easy to identify a worker-call? We can add
+-- TODO: a unique ID to the event, and the worker can include that ID in the event it sends back.
+-- TODO: But what DX makes it easy to generate and set the ID when calling from parent?
+
 send_message(e.parent_pid, {
   event = "dir_result",
   path = path,
