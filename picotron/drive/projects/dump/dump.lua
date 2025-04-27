15ea07f6116478eeb7d947b960fbbab4d9a8470a
Anders Kjær Damgaard <akd@akd.io>
2025-04-27 17:56:48 +0200

    Update dump.lua

--- picotron/drive/projects/dump/dump.lua
+++ picotron/drive/projects/dump/dump.lua
@@ -1,35 +1,52 @@
 --[[pod_format="raw",created="2025-03-31 15:08:47",modified="2025-03-31 15:08:47",revision=0]]
 
 include("/lib/describe.lua")
+include("/lib/literal.lua")
 
 local destinationArg = env().argv[1]
 assert(destinationArg == nil or type(destinationArg) == "string", "Usage: dump [destination]")
-
-print("Dumping...")
 
 local systemMetadata = fetch_metadata("/system")
 local systemVersion = systemMetadata.version
 
 local targetDir = "/dumps/" .. (destinationArg or systemVersion)
 
+print("Preparing target directory: " .. targetDir)
+rm(targetDir)
+mkdir(targetDir)
+
 -- Dump builtins (_ENV)
-rm(targetDir .. "/builtins.txt")
-create_process("/projects/builtins/builtins.lua", { argv = { targetDir .. "/builtins.txt" } })
+local builtinsPath = targetDir .. "/builtins.txt"
+rm(builtinsPath)
+create_process("/projects/builtins/builtins.lua", { argv = { builtinsPath } })
 
 -- Dump env()
-rm(targetDir .. "/env.txt")
-create_process("/projects/env/env.lua", { argv = { targetDir .. "/env.txt" } })
+local envPath = targetDir .. "/env.txt"
+rm(envPath)
+create_process("/projects/env/env.lua", { argv = { envPath } })
 
 -- Dump /system
-rm(targetDir .. "/system")
-cp("/system", targetDir .. "/system")
+local systemPath = targetDir .. "/system"
+rm(systemPath)
+cp("/system", systemPath)
 
 -- Dump /ram
-rm(targetDir .. "/ram")
-cp("/ram", targetDir .. "/ram")
+local ramPath = targetDir .. "/ram"
+rm(ramPath)
+cp("/ram", ramPath)
 
 -- Dump system metadata
-rm(targetDir .. "/system-metadata.txt")
-store(targetDir .. "/system-metadata.txt", describe(systemMetadata))
+local systemMetadataPath = targetDir .. "/system-metadata.txt"
+rm(systemMetadataPath)
+store(systemMetadataPath, describe(systemMetadata))
+
+-- Remove jettison
+include("/projects/patcher/head-remove-jettison.lua")
+
+-- Dump jailbroken builtins (_ENV without jettison)
+-- !Important: This will only work on second run, as `createProcess()` reuses the `head.lua` of this script's process!
+local builtinsJailbrokenPath = targetDir .. "/builtins-jailbroken.txt"
+rm(builtinsJailbrokenPath)
+create_process("/projects/builtins/builtins.lua", { argv = { builtinsJailbrokenPath } })
 
 print("Done.")
